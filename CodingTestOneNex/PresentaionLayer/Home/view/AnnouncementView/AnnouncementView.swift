6b57215d26a5f834cdc57869f02de855035cf4e1
Ku Ku <khukhu113@gmail.com>
2023-11-03 01:43:10 +0630

    announcementView setup

---
+++ CodingTestOneNex/PresentaionLayer/Home/view/AnnouncementView/AnnouncementView.swift
@@ -0,0 +1,55 @@
+//
+//  AnnouncementView.swift
+//  CodingTestOneNex
+//
+//  Created by Ku Ku Zan on 11/3/23.
+//
+
+import UIKit
+import UIComponents
+
+class AnnouncementView: NibBasedView {
+    
+    @IBOutlet private(set) var tableView: UITableView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.register(
+            AnnouncementItemCell.self,
+            forCellReuseIdentifier: "itemcell"
+        )
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension AnnouncementView: UITableViewDelegate {
+    
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        return tableView.estimatedRowHeight
+    }
+}
+
+extension AnnouncementView: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "itemcell",
+            for: indexPath
+        ) as! AnnouncementItemCell
+        cell.selectionStyle = .none
+        return cell
+    }
+}
---
+++ CodingTestOneNex/PresentaionLayer/Home/view/table-view-cells/AnnouncementItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnnouncementItemCell.swift
+//  CodingTestOneNex
+//
+//  Created by Ku Ku Zan on 11/3/23.
+//
+
+import UIKit
+import UIComponents
+
+class AnnouncementItemCell: NibBasedTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
