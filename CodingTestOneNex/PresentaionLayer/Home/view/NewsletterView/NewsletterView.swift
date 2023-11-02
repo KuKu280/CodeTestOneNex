4705eb4d4e344a41faf545519a746fd3d83306a1
Ku Ku <khukhu113@gmail.com>
2023-11-03 02:04:50 +0630

    newsletterview setup

---
+++ CodingTestOneNex/PresentaionLayer/Home/view/NewsletterView/NewsletterView.swift
@@ -0,0 +1,69 @@
+//
+//  NewsletterView.swift
+//  CodingTestOneNex
+//
+//  Created by Ku Ku Zan on 11/3/23.
+//
+
+import UIKit
+import UIComponents
+
+class NewsletterView: NibBasedView {
+    
+    @IBOutlet private(set) var collectionView: UICollectionView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupCollectionView()
+    }
+
+    private func setupCollectionView() {
+        collectionView.register(
+            NewsletterItemCell.self,
+            forCellWithReuseIdentifier: "itemcell"
+        )
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension NewsletterView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(
+            width: collectionView.frame.width / 3,
+            height: collectionView.frame.height
+        )
+    }
+}
+
+extension NewsletterView: UICollectionViewDataSource {
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 3
+    }
+    
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "itemcell", for: indexPath
+        ) as? NewsletterItemCell else {
+            return UICollectionViewCell()
+        }
+      
+        return cell
+    }
+    
+    
+}
---
+++ CodingTestOneNex/PresentaionLayer/Home/view/collection-view-cells/NewsletterCell/NewsletterItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  NewsletterItemCell.swift
+//  CodingTestOneNex
+//
+//  Created by Ku Ku Zan on 11/3/23.
+//
+
+import UIKit
+import UIComponents
+
+class NewsletterItemCell: NibBasedCollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+}
